df8070c687e93fea9670e9fd6bb3bb3f0c21f82a
Glitch (clock-calendar-coderdojo) <none>
2020-07-19 21:41:18 +0000

    🍱🚵 Updated with Glitch

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function updateClock() {
   var currentHours = currentTime.getHours();
   var currentMinutes = currentTime.getMinutes();
   var currentSeconds = currentTime.getSeconds();
-  
+
   currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
   currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
-  
+
   var timeOfDay = currentHours < 12 ? "AM" : "PM";
-  
+
   currentHours = currentHours > 12 ? currentHours - 12 : currentHours;
 
-currentHours = currentHours == 0 ? 12 : currentHours;
-  
-   var currentTimeString =
+  currentHours = currentHours == 0 ? 12 : currentHours;
+
+  var currentTimeString =
     currentHours +
     ":" +
     currentMinutes +
@@ -22,6 +22,6 @@ currentHours = currentHours == 0 ? 12 : currentHours;
     currentSeconds +
     " " +
     timeOfDay;
-  
+
   document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 }
